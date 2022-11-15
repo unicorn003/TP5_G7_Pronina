fdfeb168e2769d4d4607c092ed56ce19e4651365
unicorn03 <valentyna.pronina@nure.ua>
2022-11-15 23:12:20 +0100

    I fixed an error with initialisation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       SortClass sortTest = new SortClass();
+       sortTest.initialiserTableau();
+
+
+
     }
 }
--- src/SortClass.java
+++ src/SortClass.java
@@ -6,22 +6,24 @@ import java.time.Instant;
 import java.util.Random;
 
 public class SortClass {
-    private static int[] tableau;
-    Instant start;
-    // Des instructions qui peuvent prendre du temps
-    Instant end;
     static int SIZE = Integer.MAX_VALUE / 1000 ;
+    private static int[] tableau = new int[SIZE];
+    // Des instructions qui peuvent prendre du temps
+
 
     public static void initialiserTableau() {
         Instant start = Instant.now();
         System.out.println("Debut d’initialisation...");
         Random random = new Random();
-        for (int i = 0; i < tableau.length; i++) {
+        for (int i = 0; i < SIZE; i++) {
             tableau[i] = random.nextInt(SIZE);
         }
         Instant end = Instant.now();
         long duration = Duration.between(start, end).toMillis();
         System.out.println("L’initialisation a pris " + duration + " ms");
+    }
+    public static int[] getTableau(){
+        return tableau;
     }
     public static int @NotNull [] copyTableau(){
         int[] tableauSelection = new int[SIZE];
